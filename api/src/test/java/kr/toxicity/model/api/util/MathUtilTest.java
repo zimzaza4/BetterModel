8cfb621e6f64b2167c08523364dbb51f2fa8fd37
zimzaza4 <3625282098@qq.com>
2026-08-05 19:08:49 +0800

    fix: add missing license headers to fork-added files

--- api/src/test/java/kr/toxicity/model/api/util/MathUtilTest.java
+++ api/src/test/java/kr/toxicity/model/api/util/MathUtilTest.java
@@ -1,3 +1,10 @@
+/*
+ * This source file is part of BetterModel.
+ * Copyright (c) 2026 toxicity188
+ * Licensed under the MIT License.
+ * See LICENSE.md file for full license text.
+ */
+
 package kr.toxicity.model.api.util;
 
 import kr.toxicity.model.api.data.Float3;
